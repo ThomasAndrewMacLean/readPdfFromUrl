fac8a9fc997cc046001266e17ad7f80790299649
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-07-20 09:01:00 +0200

    add date

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-
-
 const pdf = require("pdf-parse");
 const https = require("https");
-
 
 let body = [];
 https
@@ -11,17 +8,28 @@ https
     (res) => {
       res.on("data", (chunk) => body.push(chunk));
       res.on("end", () => {
-        const dataBuffer = Buffer.concat(body).toString();
 
         pdf(Buffer.concat(body)).then(function (data) {
-            const getallen = data.text
+
+          console.log(data.text)
+          const getallen = data.text 
             .split("Bevestigde COVID-19 gevallen")[1]
             .split("\n")[0];
 
-            const sterf = data.text
-            .split("Sterfgevallen***")[1]
-            .split("\n")[0];
-          console.log(getallen, sterf);
+          const sterf = data.text.split("Sterfgevallen***")[1].split("\n")[0];
+
+          const datum = data.text.split("VAN")[1].split("\n")[0];
+          // console.log(getallen, sterf, datum);
+
+          const totaalGeval = getallen
+            .trim()
+            .split(" ")
+            .filter((x, i) => i < 2)
+            .join(" ");
+
+          const totaalPerc = getallen.trim().split(" ").pop();
+         
+          console.log(totaalGeval, totaalPerc);
         });
       });
     }
